9865383339676cc14eb2bfcecb6fd591970f16b3
Ryan Sandridge <noreply@ryansandridge.com>
2021-07-20 22:18:51 -0400

    Adds tests.

--- test/models/college_scorecard_test.rb
+++ test/models/college_scorecard_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class CollegeScorecardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#search without passing a block" do
+    mock = Minitest::Mock.new
+    mock.expect(:first, [:some, :data])
+
+    PaginatedRequest.stub(:new, mock) do
+      CollegeScorecard.schools.search({ 'school.name' => 'poly' })
+    end
+    assert_mock mock
+  end
+
+  test "#search with a block" do
+    mock = Minitest::Mock.new
+    mocked_data = [:some, :data]
+    mock.expect(:each, mocked_data)
+
+    PaginatedRequest.stub(:new, mock) do
+      CollegeScorecard.schools.search({ 'school.name' => 'poly' }) { |data| assert_equal(mocked_data, data) }
+    end
+    assert_mock mock
+  end
 end
